17bf2a5e3c8145ab19f0ab26937d4020bba5de9b
Tim <o.timars@gmail.com>
2019-04-12 11:19:29 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+console.log(4/0);
+console.log('string'*9);
+
+
+let something;
+console.log(something)
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone['age']);
+
+let arr = ['plum.pmj','orange.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+//alert('Че смотришь?');
+
+//let answer = confirm('Жмакай');
+
+//console.log(answer);
+
+let answer = prompt('Тебе 18?', 'Да');
+
+console.log(typeof(arr));
